504f1f6040b64d60a57f4ae2182736fecc04323b
Yulqen <y@yulqen.org>
2026-01-05 12:03:56 +0000

    build: Add CSS compilation to deploy script and remove recipe name truncation from recipe list.

--- scripts/deploy_mealplanner.sh
+++ scripts/deploy_mealplanner.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd /var/www/mealplanner/
 sudo -u www-data git pull
+sudo -u www-data make css
 sudo -u www-data /var/www/.local/bin/uv run python manage.py migrate
 sudo -u www-data /var/www/.local/bin/uv run python manage.py collectstatic --noinput
-supervisorctl restart mealplanner
+sudo supervisorctl restart mealplanner
 
 # this is in /usr/local/bin/ on the server and can be run from the local machine. See wekan.
