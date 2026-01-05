55d5ac036bf557f70390e9d1321ffbb979bc228b
Yulqen <y@yulqen.org>
2026-01-05 10:35:03 +0000

    Adds the deploy script

---
+++ scripts/deploy_mealplanner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /var/www/mealplanner/
+sudo -u www-data git pull
+sudo -u www-data /var/www/.local/bin/uv run python manage.py migrate
+sudo -u www-data /var/www/.local/bin/uv run python manage.py collectstatic --noinput
+supervisorctl restart mealplanner
+
+# this is in /usr/local/bin/ on the server and can be run from the local machine. See wekan.
